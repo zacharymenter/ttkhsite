ded0b362d9fe29f2ee1ea6931839056a05ff2891
menterzj3144 <47538306+menterzj3144@users.noreply.github.com>
2020-03-03 11:26:23 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
 if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+    mail("zachary_menter@hotmail.com", "Form to email message", $_POST["message"], "From: zachary_menter@hotmail.com");
 }
 ?>

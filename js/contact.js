53b1c672d24bd1acdc1501ff5867a064d6d5d6ef
menterzj3144 <menterzj3144@uwec.edu>
2020-06-25 00:02:14 -0500

    updates

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,6 @@
 const submit = document.getElementById('submit-button');
-const reset = document.getElementById('reset-button');
 const paragraph = document.getElementById('thank');
 
 submit.addEventListener('click', () => {
     paragraph.style.display = 'block';
-});
-
-reset.addEventListener('click', () => {
-    paragraph.style.display = 'none';
 });

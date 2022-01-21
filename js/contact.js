cc2fc6e931448fdbee05e3f197b296685972d24e
Zachary Menter <menterzj3144@uwec.edu>
2022-01-21 10:53:50 -0600

    Remove send email button
    It didn't really work

--- js/contact.js
+++
@@ -1,6 +0,0 @@
-const submit = document.getElementById('submit-button');
-const paragraph = document.getElementById('thank');
-
-submit.addEventListener('click', () => {
-    paragraph.style.display = 'block';
-});
